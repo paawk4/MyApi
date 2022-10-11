6be2933a6963c60ebcb2e4f81c7cdaa3c119ae18
ЧетвериковПВ <@105-01>
2022-10-11 09:30:10 +0300

    Добавьте файлы проекта.

---
+++ MyApi/Controllers/Personal_InfController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MyApi.Entities;
+using MyApi.Models;
+
+namespace MyApi.Controllers
+{
+    public class Personal_InfController : ApiController
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: api/Personal_Inf
+        [ResponseType(typeof(List<ProfileModel>))]
+        public IHttpActionResult GetProfiles()
+        {
+            return Ok(db.Personal_Inf.ToList().ConvertAll(x => new ProfileModel(x)));
+        }
+
+        // GET: api/Personal_Inf/5
+        [ResponseType(typeof(Personal_Inf))]
+        public IHttpActionResult GetPersonal_Inf(int id)
+        {
+            Personal_Inf personal_Inf = db.Personal_Inf.Find(id);
+            if (personal_Inf == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(personal_Inf);
+        }
+
+        // PUT: api/Personal_Inf/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPersonal_Inf(int id, Personal_Inf personal_Inf)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != personal_Inf.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(personal_Inf).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Personal_InfExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Personal_Inf
+        [ResponseType(typeof(Personal_Inf))]
+        public IHttpActionResult PostPersonal_Inf(Personal_Inf personal_Inf)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Personal_Inf.Add(personal_Inf);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = personal_Inf.id }, personal_Inf);
+        }
+
+        // DELETE: api/Personal_Inf/5
+        [ResponseType(typeof(Personal_Inf))]
+        public IHttpActionResult DeletePersonal_Inf(int id)
+        {
+            Personal_Inf personal_Inf = db.Personal_Inf.Find(id);
+            if (personal_Inf == null)
+            {
+                return NotFound();
+            }
+
+            db.Personal_Inf.Remove(personal_Inf);
+            db.SaveChanges();
+
+            return Ok(personal_Inf);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool Personal_InfExists(int id)
+        {
+            return db.Personal_Inf.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ MyApi/Models/ProfileModel.cs
@@ -0,0 +1,26 @@
+﻿using MyApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApi.Models
+{
+    public class ProfileModel
+    {
+        public ProfileModel(Personal_Inf profile)
+        {
+            Id = profile.id;
+            Name = profile.name;
+            Job = profile.job;
+            Email = profile.email;
+            Image = profile.image;
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public string Email { get; set; }
+        public string Image { get; set; }
+    }
+}
